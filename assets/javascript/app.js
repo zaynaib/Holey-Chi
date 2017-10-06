0856605c0523b595da224f28ad0385d6e347a94b
jason.terrell@gmail.com <jason.terrell@gmail.com>
2017-10-06 10:38:35 -0500

    favicon and location for chicago

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,11 +10,11 @@ $(document).ready(function(){ // everything goes between these
 		}
 
 	}).done(function(data) {
-		alert("Retrieved " + data.length + " records from the dataset!");
+		// alert("Retrieved " + data.length + " records from the dataset!");
 		console.log(data);
 	});
 
-	var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+	var mymap = L.map('mapid').setView([41.8781, -87.6298], 15);
 
 	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
 		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
