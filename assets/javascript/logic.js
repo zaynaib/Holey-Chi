144e8e490d85f67605df93081c30fb00d17dc867
Kaitlyn <kaitlyn.strand@gmail.com>
2017-10-08 21:58:43 -0500

    weekend

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -7,7 +7,7 @@ $(document).ready(function(){ // everything goes between these
 		url: "https://data.cityofchicago.org/resource/787j-mys9.json",
 		type: "GET",
 		data: {
-			"$limit" : 1000,
+			"$limit" : 5,
 			"$$app_token" : "rWk97H84NMWrBWcdiG4IvjTjX"
 		}
 
@@ -33,24 +33,13 @@ $(document).ready(function(){ // everything goes between these
 		accessToken: 'pk.eyJ1Ijoia2FpdGx5bnN0cmFuZCIsImEiOiJjajhlcmwweWgxNjkzMzNwbTBub3ZuN3FxIn0.1Nz-cdZ8Ew7Oa3dxqxzdaQ'
 	}).addTo(mymap);
 
-	var potholeOpen = new L.Icon({
-      iconUrl: 'assets/images/icon-red.png',
+	var redIcon = new L.Icon({
+      iconUrl: 'http://leafletjs.com/examples/custom-icons/leaf-orange.png',
       shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
-      
       iconSize: [25, 41],
       iconAnchor: [12, 41],
       popupAnchor: [1, -34],
-      shadowSize: [41, 41],
-
-  })
-
-   var potholeClosed = new L.Icon({
-        iconUrl: 'assets/images/icon-green.png',
-      	shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
-      	iconSize: [25, 41],
-      	iconAnchor: [12, 41],
-      	popupAnchor: [1, -34],
-      	shadowSize: [41, 41]
+      shadowSize: [41, 41]
     });
 
     for(var i =0 ; i < data.length; i++){
@@ -58,35 +47,17 @@ $(document).ready(function(){ // everything goes between these
       console.log(data[i].latitude);
         var dataLong = data[i].longitude;
         console.log(data[i].longitude);
-        var dataStatus = data[i].status;
-        console.log(data[i].status);
-        
-        var dataAddress = data[i].street_address;
-        console.log(data[i].street_address);
-        var dataAction = data[i].most_recent_action;
-        console.log(data[i].most_recent_action);
-
-        if (dataStatus === "Completed") {
-        	console.log([dataLat, dataLong], "this  is the info")
-        	L.marker([dataLat, dataLong], {icon: potholeClosed}).addTo(mymap).bindPopup("<b>" + data[i].street_address + "</b><br>" + data[i].most_recent_action)
-            .openPopup();
-
-        }
-        else {
-        	 L.marker([dataLat, dataLong], {icon: potholeOpen}).addTo(mymap).bindPopup("<b>" + data[i].street_address + "</b><br>" + data[i].most_recent_action)
-            .openPopup();
-        }
-
         //var marker = L.marker([dataLat,dataLong]).addTo(mymap);
-       
-        
-
+        L.marker([dataLat, dataLong], {icon: redIcon}).addTo(mymap);
     }
 
-
 }); // $(document).ready(function(){});
-});
- 
+
+	});
+
+	
+	
+// 
 // take string from user entry and .toUppercase 
 //create for loop through the "street address" of pothole information and see 
 //equal to user entry
